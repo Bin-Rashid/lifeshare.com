a31c03559d7aa49a35230b3a062b51d0b75c1633
ShawonBinRashid <shawonbinrashid@gmail.com>
2025-11-25 01:43:31 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': __dirname,
-    };
+  webpack: (config, { isServer }) => {
+    // Handle private class fields in undici
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { 
+              targets: { 
+                node: '18' 
+              } 
+            }]
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-private-methods'
+          ]
+        }
+      }
+    });
+
     return config;
   },
+  // Remove experimental config as it's not needed in Next.js 14
 }
 
 module.exports = nextConfig
