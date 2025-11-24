311d14a106bbf8151d31e21e36338aa84f0de4d2
ShawonBinRashid <shawonbinrashid@gmail.com>
2025-11-25 01:27:43 +0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
