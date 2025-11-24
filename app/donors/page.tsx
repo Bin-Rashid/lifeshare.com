d2d1a085cade61d6e51022cbdd9616e202858957
ShawonBinRashid <shawonbinrashid@gmail.com>
2025-11-25 01:26:37 +0600

    Update page.tsx

--- app/donors/page.tsx
+++ app/donors/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import DonorCard from '@/components/DonorCard';
-import { User } from '@/types';
-import { getAllDonors } from '@/lib/firestore';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import DonorCard from '../../components/DonorCard';
+import { User } from '../../types';
+import { getAllDonors } from '../../lib/firestore';
 
 export default function DonorsPage() {
   const [donors, setDonors] = useState<User[]>([]);
